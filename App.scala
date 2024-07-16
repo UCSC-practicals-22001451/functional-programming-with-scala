245c870c3d16e6399d0dd1dbf053a69760dcb456
Savith Panamgama <geethikasavith@gmail.com>
2024-07-16 12:30:17 +0530

    update: remove comments

--- App.scala
+++ App.scala
@@ -6,8 +6,7 @@ object App {
         case "p5e1" => practical_5.inventory_management.run()
         case "p5e2" => practical_5.library_management.run()
         case "p5e3" => practical_5.print_fib.run()
-        // Add cases for other exercises here
-        case _ => println("Exercise not found.")
+        case _      => println("Exercise not found.")
       }
     } else {
       println("Please specify an exercise to run.")
